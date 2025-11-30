c5243a1b56727f10043055e3d70b94eb82927c8a
VisionsXZ <64665287+VisionsXZ@users.noreply.github.com>
2025-11-30 23:55:55 +0400

    Upload Files

---
+++ Sleak Crypter/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sleak Crypter Tool")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sleak Crypter Tool")]
+[assembly: AssemblyCopyright("© Sleak Corporation. All rights reserved")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5CE578F1-E7DC-41A0-8798-E62ABEA2B6E7")]
+[assembly: AssemblyVersion("1.80.383.5940")]
+[assembly: AssemblyFileVersion("1.80.383.5940")]
